c25917687f0afb1c1653585db9236e807e02a137
Jasmine Ryon <jryon12@priorypride.net>
2021-04-25 14:21:42 -0700

    commit tree cb2579414ff4e979c9387467c13d073924d69230

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    @@pirates = []
+
+    def initialize(params)
+        @name = params[:name]
+        @type = params[:weight]
+        @booty = params[:height]
+        PIRATES << self
+    end
+
+    def self.all
+        @@pirates
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+    @@ships = []
+
+    def initialize(params)
+        @name = params[:name]
+        @type = params[:type]
+        @booty = params[:booty]
+        SHIPS << self
+    end
+
+    def self.all
+        @@ships
+    end
+
+    def self.clear
+        @@ships = []
+    end
+
 end
